ead4186ca0e5142e8845ad7aa78c2c3b88399ff5
hendisantika <hendisantika@yahoo.co.id>
2024-07-22 18:30:07 +0700

    Add app/api register.js 🫸🌀✏️📗🐧🐳

---
+++ app/api/register.js
@@ -0,0 +1,19 @@
+const register = async (req, res) => {
+    const SECRET_KEY = process.env.RECAPTCHA_SECRETKEY;
+
+    const {name, email, recaptchaResponse} = req.body;
+
+    const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${SECRET_KEY}&response=${recaptchaResponse}`;
+
+    try {
+        const recaptchaRes = await fetch(verifyUrl, {method: "POST"});
+
+        const recaptchaJson = await recaptchaRes.json();
+
+        res.status(200).json({name, email, ...recaptchaJson});
+    } catch (e) {
+        res.status(400).json(e.error);
+    }
+};
+
+export default register;
